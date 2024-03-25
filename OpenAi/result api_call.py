dd93e88994fb42fb5e4b154fd32256c6fe712c43
sufuang <36677078+sufuang@users.noreply.github.com>
2024-03-25 11:14:32 -0700

    Add files via upload

---
+++ OpenAi/result api_call.py
@@ -0,0 +1,27 @@
+
+(base) C:\Users\sophia.yue\AI>python api_call.py
+Length:  35
+
+0
+Item name-link: "10 words or less."
+Item name-img : "10 words or less."
+Flavor-link: "10 words or less."
+Flavor-img : "10 words or less."
+Product Details: "The product details of the item in this page are as follows:\n\n* Brand: Drumstick\n* Product Name: Vanilla Frozen Dairy Dessert Cones\n* Flavor: Vanilla\n* Type: Frozen Dairy Dessert\n* Packaging: Box\n* Number of Cones: 8\n* Weight: 1.5 lbs\n* Ingredients: Vanilla ice cream, sugar, cream, milk, and vanilla flavor.\n* Allergens: Milk, cream, and vanilla flavor.\n* Dietary Restrictions: Contains dairy and vanilla flavor.\n* Storage: Keep frozen.\n* Shelf Life: 1 year.\n* Country of Origin: United States."
+Flavor: "The product details of the item in this page are as follows:\n\n* Brand: Drumstick\n* Type: Frozen Dairy Dessert Cones\n* Flavor: Vanilla\n* Packaging: Box of 8 cones\n* Ingredients: Vanilla ice cream, sugar, cream, and vanilla flavoring\n* Nutritional information: 1 cone contains 120 calories, 4.5 grams of fat, 1.5 grams of saturated fat, 0 grams of trans fat, 10 milligrams of cholesterol, 100 milligrams of sodium, 25 grams of carbohydrates, and 1 gram of protein."
+
+1
+Item name-link: "10 words or less."
+Item name-img : "10 words or less."
+Flavor-link: "10 words or less."
+Flavor-img : "10 words or less."
+Product Details: "The product details of the item in this page are as follows:\n\n* Brand: Kroger\n* Product Name: Deluxe Mint Chocolate Chip Ice Cream\n* Package Type: Container\n* Package Size: 1.5 quarts\n* Flavor: Mint Chocolate Chip\n* Ingredients: Cream, sugar, corn syrup, whey protein concentrate, cocoa, mint, and other natural flavors.\n* Allergens: Milk, eggs, and wheat.\n* Manufacturer: Kroger\n* Country of Origin: United States\n* Dimensions: 7.5 x 7.5 x 7.5 inches\n* Weight: 2.5 pounds\n* Servings: 16\n* Storage: Keep frozen.\n* Best if used by date: See package for expiration date."
+Flavor: "The product details of the item in this page are:\n\n* Brand: Kroger\n* Name: Deluxe Mint Chocolate Chip Ice Cream\n* Type: Ice Cream\n* Flavor: Mint Chocolate Chip\n* Size: 1.5 quart\n* Packaging: Plastic container\n* Ingredients: Cream, sugar, cocoa, mint, and other natural flavors\n* Nutritional Information: Calories per serving: 240, Fat: 12g, Saturated Fat: 6g, Cholesterol: 30mg, Carbohydrates: 28g, Fiber: 0g, Sugar: 24g, Protein: 4g, Vitamin A: 600 IU, Vitamin C: 0mg, Calcium: 150mg, Iron: 0mg"
+
+2
+Item name-link: "10 words or less"
+Item name-img : "10 words or less."
+Flavor-link: "10 words or less."
+Flavor-img : "10 words or less."
+Product Details: "The product details of the item in this page are as follows:\n\n* Brand: Kroger\n* Product Name: Deluxe Triple Brownie Ice Cream\n* Type: Ice Cream\n* Flavor: Triple Brownie\n* Container: Tub\n* Size: 1.5 quarts\n* Weight: 1.5 pounds\n* Ingredients: Cream, sugar, cocoa, butter, wheat flour, eggs, chocolate, milk, nuts, vanilla, and caramel\n* Allergens: Milk, eggs, wheat, nuts, and caramel\n* Manufacturer: Kroger\n* Country of Origin: United States\n* Dietary Restrictions: Lactose intolerant, gluten intolerant, and nut allergies\n* Storage: Keep frozen at or below 0°F (-18°C)\n* Shelf Life: 1 year from the date of manufacture\n* Serving Size: 1/2 cup (60 grams)\n* Servings Per Container: 12\n* Amount Per Serving: 150 calories, 8 grams of fat, 1 gram of saturated fat, 0 grams of trans fat, 150 milligrams of cholesterol, 100 milligrams of sodium, 25 grams of carbohydrates, 1 gram of dietary fiber, 15 grams of sugar, and 4 grams of protein."
+Flavor: "The product details of the item in this page are:\n\n* Brand: Kroger\n* Product Name: Deluxe Triple Brownie Ice Cream\n* Package Type: Tub\n* Size: 1.5 quarts\n* Flavor: Triple Brownie\n* Ingredients: Cream, sugar, skim milk, cocoa, butter, whey, nonfat milk, chocolate, and vanilla\n* Nutrition Facts: 1/2 cup serving size, 240 calories, 12g fat, 5g saturated fat, 0g trans fat, 100mg cholesterol, 100mg sodium, 30g carbohydrates, 2g dietary fiber, 16g sugars, 4g protein\n* Allergens: Milk, wheat, eggs, and soy\n* Manufacturer: Kroger\n* Country of Origin: United States"
